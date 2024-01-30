07a8b69f69265843dd5592473e3e157247a8c85d
CesarBazan23 <cbazan724@gmail.com>
2024-01-30 17:19:09 -0300

    adição de calculadora e calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+function soma(a, b){
+    return a+b
+}
+function sub(a, b){
+    return a-b
+}
+function mult(a, b){
+    return a*b
+}
+function div(a, b){
+    return b>0? a/b : "Não é possível dividir por 0."
+}
